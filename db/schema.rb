c9ac1865fd3c1df866ccd1805b8b069831a4cf22
William Ryan O'Sullivan <wrosullivan88@gmail.com>
2024-02-14 18:42:08 -0600

    Comment model g - forgot to switch branches

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_14_233717) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_15_004048) do
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "link_url"
     t.integer "user_id"
